940215a5b8edf133bab2cc033d7646eb5d76eb58
me <irfankf@gmail.com>
2024-10-23 14:06:40 +0700

    after [`ffbuildSubstituteHeaders/config.h`] these additional fixups (missing those zero-ing `#define`s)

--- r-find-all-cpps.bat
+++ r-find-all-cpps.bat
@@ -2,10 +2,25 @@
 
 
 
+@REM @echo off
 
 
 
-J:\Dev\StuDKitSrc\scripts\exp\FindCpps.bat --base-dir=J:\Dev\ff202303 J:\Dev\ff202303\fftools J:\Dev\ff202303\libavutil
+@REM cd %~dp0
+
+
+@REM @echo "== fftools =="
+@REM %~dp0\r-find-specific-cpps.bat J:\Dev\ff202303\fftools  
+
+@REM @echo "== libavutil =="
+@REM %~dp0\r-find-specific-cpps.bat  J:\Dev\ff202303\libavutil
+
+@REM @echo "== libavformat =="
+@REM %~dp0\r-find-specific-cpps.bat  J:\Dev\ff202303\libavformat
+
+%~dp0\r-find-specific-cpps.bat J:\Dev\ff202303\fftools  J:\Dev\ff202303\libavutil J:\Dev\ff202303\libavformat
+
+@REM r-find-specific-cpps.bat  J:\Dev\ff202303\libavcodec
 
 
 
