88c511215477e7c85c7a0da7166e599a204ef45d
me <irfankf@gmail.com>
2024-10-27 18:32:48 +0700

    after [`configure`-related setup scripts ] these tryout/test-related chgs

---
+++ r-test-ecs.bat
@@ -0,0 +1,19 @@
+
+
+
+
+
+node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i testsrc2=duration=5.1 -f null -c:v rawvideo myvideo
+
+
+
+node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 file:///J:/Dev/alqwertyuiopasdfghjkl1.webm
+
+
+
+
+node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 pipe: > J:\Dev\alqwertyuiopasdfkl1.webm
+
+
+
+
