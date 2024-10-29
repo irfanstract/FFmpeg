a93f694ad4d3ccd9fa1c401c870bb467e6b7b9d5
me <irfankf@gmail.com>
2024-10-29 13:38:17 +0700

    after [Postbuild Script(s) ] these chgs

--- r-test-ecs.bat
+++ r-test-ecs.bat
@@ -7,12 +7,12 @@ node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i testsrc2=dur
 
 
 
-node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 file:///J:/Dev/alqwertyuiopasdfghjkl1cE.webm
+node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 file:///J:/Dev/alqwertyuiopasdfghjkl1cEj.webm
 
 
 
 
-node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 pipe: > J:\Dev\alqwertyuiopasdfkl1cE.webm
+node .\build\ff-ranwithemcp\ffmpeg.js -loglevel verbose -f lavfi -i "testsrc2=duration=5.1:size=352x288,realtime" -f matroska -c:v h263 -flush_packets 1 pipe: > J:\Dev\alqwertyuiopasdfkl1cEj.webm
 
 
 
